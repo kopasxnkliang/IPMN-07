616dc08ab37d43643e0decdcea1a47240226b581
kopasxnkliang <63278285+kopasxnkliang@users.noreply.github.com>
2023-06-01 22:22:03 +0800

    Update dataset.py

--- example/dataset.py
+++ example/dataset.py
@@ -4,12 +4,12 @@ dataset1 = load_dataset("nickmuchi/trade-the-event-finance")
 
 dataset2 = load_dataset("PaulAdversarial/all_news_finance_sm_1h2023")
 
-dataset5 = datasets.load_dataset("JanosAudran/financial-reports-sec", "large_lite")
+dataset5 = load_dataset("JanosAudran/financial-reports-sec", "large_lite")
 
-dataset4 = datasets.load_dataset("nlpaueb/finer-139", split="train")
+dataset4 = load_dataset("nlpaueb/finer-139", split="train")
 
 dataset3 = load_dataset("financial_phrasebank")
 
-# dataset6 & 7 from kaggle (csv / json*8)
+# dataset6 & 7 from kaggle (csv / json*8) see google doc
 
 # dataset8 crime news
