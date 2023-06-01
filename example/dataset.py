19f4374bfa71c60422cc7182a51e77365a3a1136
kopasxnkliang <63278285+kopasxnkliang@users.noreply.github.com>
2023-06-01 21:46:26 +0800

    Create dataset.py

---
+++ example/dataset.py
@@ -0,0 +1,15 @@
+from datasets import load_dataset
+
+dataset1 = load_dataset("nickmuchi/trade-the-event-finance")
+
+dataset2 = load_dataset("PaulAdversarial/all_news_finance_sm_1h2023")
+
+dataset5 = datasets.load_dataset("JanosAudran/financial-reports-sec", "large_lite")
+
+dataset4 = datasets.load_dataset("nlpaueb/finer-139", split="train")
+
+dataset3 = load_dataset("financial_phrasebank")
+
+# dataset6 & 7 from kaggle (csv / json*8)
+
+# dataset8 crime news
